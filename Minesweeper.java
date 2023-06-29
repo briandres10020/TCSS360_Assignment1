8ae72302a8b2845aef9fe9b5f978301891c1e2b7
Alex Bell <125619634+Alex1715UW@users.noreply.github.com>
2023-06-29 14:38:54 -0700

    Made code more modular. Better for unit testing.

--- Minesweeper.java
+++ Minesweeper.java
@@ -8,24 +8,29 @@ import java.util.Scanner;
 
 /**
  * Minesweeper class represents a game of Minesweeper.
+ * @author Jerry
+ * @version 1.0
  */
-public class Minesweeper {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public final class Minesweeper {
+    private Minesweeper() {
+        super();
+    }
+    public static void main(final String[] theArgs) {
+        final Scanner scanner = new Scanner(System.in);
         int fieldCount = 1;
 
         while (true) {
-            int n = scanner.nextInt();
-            int m = scanner.nextInt();
+            final int n = scanner.nextInt();
+            final int m = scanner.nextInt();
             scanner.nextLine();
 
             if (n == 0 && m == 0) {
                 break;
             }
 
-            char[][] field = new char[n][m];
+            final char[][] field = new char[n][m];
             for (int i = 0; i < n; i++) {
-                String line = scanner.nextLine();
+                final String line = scanner.nextLine();
                 for (int j = 0; j < m; j++) {
                     field[i][j] = line.charAt(j);
                 }
@@ -35,7 +40,7 @@ public class Minesweeper {
                 System.out.println();
             }
             System.out.println("Field #" + fieldCount + ":");
-            calculateAdjacentMines(field);
+            calculateMineValues(field);
 
             fieldCount++;
         }
@@ -46,27 +51,38 @@ public class Minesweeper {
      * This method calculates the number of adjacent mines for each safe square
      * in the given field and updates the field in-place. Then it prints the field.
      *
-     * @param field the 2D array representing the field
+     * @param theField the 2D array representing the field
      */
-    private static void calculateAdjacentMines(char[][] field) {
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[0].length; j++) {
-                if (field[i][j] != '*') {
-                    int count = 0;
-                    for (int k = -1; k <= 1; k++) {
-                        for (int l = -1; l <= 1; l++) {
-                            int ni = i + k;
-                            int nj = j + l;
-                            if (ni >= 0 && nj >= 0 && ni < field.length && nj < field[0].length && field[ni][nj] == '*') {
-                                count++;
-                            }
-                        }
-                    }
-                    field[i][j] = (char) (count + '0');
-                }
-                System.out.print(field[i][j]);
+    private static void calculateMineValues(final char[][] theField) {
+        for (int row = 0; row < theField.length; row++) {
+            for (int column = 0; column < theField[0].length; column++) {
+                theField[row][column] = Character.forDigit(
+                        adjacentMines(theField, row, column), 10);
+                System.out.print(theField[row][column]);
             }
-            System.out.println();
         }
+        System.out.println();
+
+
     }
+    private static int adjacentMines(final char[][] theField,
+                                     final int theRow, final int theColumn) {
+        int result = 0;
+        if (theField[theRow][theColumn] != '*') {
+            for (int k = -1; k <= 1; k++) {
+                for (int l = -1; l <= 1; l++) {
+                    final int ni = theRow + k;
+                    final int nj = theColumn + l;
+                    if (ni >= 0 && nj >= 0 && ni < theField.length
+                            && nj < theField[0].length && theField[ni][nj] == '*') {
+                        result++;
+                    }
+                }
+            }
+
+        }
+        return result;
+    }
+
+
 }
