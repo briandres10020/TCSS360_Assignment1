86eda40f98165a9de9bbb39c8908e8c53d3f41d9
briannaandres <bandres@uw.edu>
2023-07-01 00:27:23 -0700

    Finishing up

--- src/MineSweeperBrianna.java
+++ src/MineSweeperBrianna.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class MineSweeperBrianna {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int FieldNum =1;
+        int FieldNum = 1;
 
         while (true){
             //N is columns
@@ -19,24 +19,41 @@ public class MineSweeperBrianna {
             //M is Rows
             int MRows = scanner.nextInt();
 
+            scanner.nextLine();
+
             //if row is equal to zero
-            if(MRows == 0){
-                //if it equals to zero, then break
-                break;
-            }
-            //if column is equal to zero
-            if(NCol == 0) {
+            if(MRows == 0 && NCol == 0){
                 //if it equals to zero, then break
                 break;
             }
             //If the field number is greater than one
             if (FieldNum > 1){
                 //if it is, print out new line
-                System.out.println(" \n ");
+                System.out.println("");
             }
             //creating a 2d array of Row and Columns
             char[][] Board = new char[MRows][NCol];
+
+            //for loop to read Board
+            for (int i = 0; i < MRows; i++) {
+                final String line = scanner.nextLine();
+                for (int j = 0; j < NCol; j++) {
+                    Board[i][j] = line.charAt(j);
+                }
+            }
+            //calling Board of solveMine
+            solveMine(Board);
+
+            System.out.println("Field #" + FieldNum + ":");
+
             FieldNum++;
+
+            for (int row = 0; row < Board.length; row++) {
+                for (int column = 0; column < Board[0].length; column++) {
+                    System.out.print(Board[row][column]);
+                }
+                System.out.println();
+            }
         }
     }
 
@@ -45,17 +62,26 @@ public class MineSweeperBrianna {
         //for loop for MRows
     for (int a = 0; a < Board.length; a++){
         //for loop for NCol
-            for( int b = 0; b < Board[0].length; b++){
+            for(int b = 0; b < Board[0].length; b++){
+                //If it is NOT IsItAMine
                 if(!IsItAMine(a,b,Board)){
+                    //Run Surroundings method
+                    int Mines = Surroundings(a,b,Board);
+                    //maybe vvv
+                    //char v = (char) ('0'+ Mines);
+
+                    //Setting 2d array format
+                    Board[a][b] = Character.forDigit(Mines, 10);
+                    }
                 }
                 }
             }
-    }
+
 
     //Method to check around board
     public static int Surroundings(int MRows, int NCol, char Board[][]){
         int result = 0;
-        //is statement: row - 1 < Board.length and NCol - 1 < Board[0].length to check
+        //if statement: row - 1 < Board.length and NCol - 1 < Board[0].length to check
         //if cell is in range
 
         //Prev Row&Col
