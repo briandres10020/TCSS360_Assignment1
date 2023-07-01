5284d4099caed384bc217a069e6ca1577d4c45ef
Alex Bell <125619634+Alex1715UW@users.noreply.github.com>
2023-06-30 18:53:11 -0700

    upload

---
+++ src/MineSweeper.java
@@ -0,0 +1,109 @@
+public class MineSweeper {
+    /**
+     *
+     */
+    private static final int MAX_SIZE = 100;
+    /**
+     *
+     */
+    private static final int MIN_SIZE = 1;
+    /**
+     *
+     */
+    private final char[][] myMineField;
+    /**
+     *
+     */
+    private final int myRows;
+    /**
+     *
+     */
+    private final int myColumns;
+    /**
+     *
+     */
+    private boolean myMineFieldSet;
+    public MineSweeper(final int theRow, final int theCol) {
+        if (theRow < MIN_SIZE || MAX_SIZE < theRow) {
+            throw new IllegalArgumentException(
+                    "given number of Rows is out of acceptable range");
+        }
+        if (theCol < MIN_SIZE || MAX_SIZE < theCol) {
+            throw new IllegalArgumentException(
+                    "given number of Columns is out of acceptable range");
+        }
+        myMineField = new char[theRow + 2][theCol + 2];
+        myRows = theRow;
+        myColumns = theCol;
+        myMineFieldSet = false;
+    }
+    public void mineFieldSet(final char[][] theMineField) {
+        if (theMineField.length != myRows || theMineField[0].length != myColumns) {
+            throw new IllegalArgumentException(
+                    "The MineField provided does not not match the specified size");
+        }
+        for (int row = 0; row < myRows; row++) {
+            for (int col = 0; col < myColumns; col++) {
+                myMineField[row + 1][col + 1] = theMineField[row][col];
+            }
+        }
+        myMineFieldSet = true;
+    }
+    public char[][] solve() {
+        if (!myMineFieldSet) {
+            throw new IllegalStateException("Cannot solve a minefield which has not been set");
+        }
+        final char[][] solution = new char[myRows][myColumns];
+        for (int row = 0; row < myRows; row++) {
+            for (int col = 0; col < myColumns; col++) {
+                if (myMineField[row + 1][col + 1] == '*') {
+                    solution[row][col] = '*';
+                } else {
+                    solution[row][col] = Character.forDigit(sweep(row + 1, col + 1),10);
+                }
+            }
+        }
+        return solution;
+    }
+    private int sweep(final int theRow, final int theColumn) {
+        int result = 0;
+
+        //Check row above
+        if (myMineField[theRow + 1][theColumn - 1] == '*') {
+            result++;
+        }
+        if (myMineField[theRow + 1][theColumn] == '*') {
+            result++;
+        }
+
+        if (myMineField[theRow + 1][theColumn + 1] == '*') {
+            result++;
+        }
+
+        //Check row below
+        if (myMineField[theRow - 1][theColumn - 1] == '*') {
+            result++;
+        }
+        if (myMineField[theRow - 1][theColumn] == '*') {
+            result++;
+        }
+
+        if (myMineField[theRow - 1][theColumn + 1] == '*') {
+            result++;
+        }
+
+
+        //Check current row
+        if (myMineField[theRow][theColumn - 1] == '*') {
+            result++;
+        }
+
+
+        if (myMineField[theRow][theColumn + 1] == '*') {
+            result++;
+        }
+        return result;
+    }
+}
+
+
---
+++ src/MineSweeperMain.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public final class MineSweeperMain {
+    private MineSweeperMain() {
+        super();
+    }
+    public static void main(final String[] theArgs) {
+        final Scanner input = new Scanner(System.in);
+        int currentField = 1;
+        while (true) {
+            final int  rows = input.nextInt();
+            final int columns = input.nextInt();
+            input.nextLine();
+            if (rows == 0 && columns == 0) {
+                break;
+            }
+
+            final char[][] fieldInput = new char[rows][columns];
+            for (int i = 0; i < rows; i++) {
+                final String line = input.nextLine();
+                for (int j = 0; j < columns; j++) {
+                    fieldInput[i][j] = line.charAt(j);
+                }
+            }
+            final MineSweeper sweeper = new MineSweeper(rows, columns);
+            sweeper.mineFieldSet(fieldInput);
+
+            if (currentField > 1) {
+                System.out.println();
+            }
+            System.out.println("Field #" + currentField + ":");
+            currentField++;
+
+            final char[][] fieldOutput = sweeper.solve();
+            for (int row = 0; row < fieldOutput.length; row++) {
+                for (int column = 0; column < fieldOutput[0].length; column++) {
+                    System.out.print(fieldOutput[row][column]);
+                }
+                System.out.println();
+            }
+        }
+    }
+
+}
